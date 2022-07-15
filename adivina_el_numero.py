f91441d56f49ad3b37415a14e93f5dc311465df0
Juan Jose <jjquintero0928@gmail.com>
2022-07-15 12:38:40 -0500

    Se agrega funcionalidad al archivo adivina_el_numero

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -2,17 +2,18 @@ import random
 
 def run():
     aleatorio = random.randint(1,100)
-    numero_elegido = int(input("Elige un numero del 1 al 100 : "))
-
-    while numero_elegido != aleatorio:
-        if numero_elegido < aleatorio:
-            print("Busca un numero mas grande")
-        else:
-            print("Busca un numero mas pequeño")
-        numero_elegido = int(input("Elige otro numero : "))
-    print("GANASTE")
-
-
+    opcion = int(input("Elige Opcion:  1- Jugar   2-Salir  : "))
+    while opcion == 1:
+        numero_elegido = int(input("Elige un numero del 1 al 100 : "))
+        while numero_elegido != aleatorio:
+            if numero_elegido < aleatorio:
+                print("Busca un numero mas grande")
+            else:
+                print("Busca un numero mas pequeño")
+            numero_elegido = int(input("Elige otro numero : "))
+        print("GANASTE")
+        opcion = int(input("Elige Opcion:  1- Jugar   2-Salir : "))
+    print("Saliendo...")
 
 if __name__ == '__main__':
     run()
